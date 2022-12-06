4b941cfd6580cc2ff16906fd185c14b68dd3e0a4
Harish B <harish,bakthavatchalu@cognizant.com>
2022-12-06 17:59:03 +0530

    adding sample blockchain db

--- service/routes/users.js
+++ service/routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var sampleRepo = require('../repos/sample_repo.js');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  sampleRepo.postData()
   res.send('respond with a resource');
 });
 
